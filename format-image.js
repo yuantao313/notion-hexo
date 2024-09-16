a08a38a908c1f428263ee606b83e7dac4ea31832
yuantao <yuantao313@outlook.com>
2024-09-16 16:38:54 +0800

    fix cover

--- format-image.js
+++ format-image.js
@@ -1,6 +1,6 @@
 
 const { matterMarkdownAdapter } = require('@elog/cli')
-
+const elogConfig = require('./elog.config');
 /**
  * 自定义文档处理器
  * @param {DocDetail} doc doc的类型定义为 DocDetail
@@ -8,9 +8,12 @@ const { matterMarkdownAdapter } = require('@elog/cli')
  * @return {Promise<DocDetail>} 返回处理后的文档对象
  */
 const format = async (doc, imageClient) => {
+  if (!elogConfig.deploy.local.frontMatter.include.includes('cover')) {
+    return doc;
+  }
   const cover = doc.properties.cover
   // 将 cover 字段中的 notion 图片下载到本地
-  if (imageClient)  {
+  if (imageClient) {
     // 只有启用图床平台image.enable=true时，imageClient才能用，否则请自行实现图片上传
     const url = await imageClient.uploadImageFromUrl(cover, doc)
     // cover链接替换为本地图片
